1de0a2da5f298f2c9e899803abdb155e8a4657c6
Styaa <91019301+Styaa@users.noreply.github.com>
2025-06-18 23:39:12 +0700

    Update login.jsx

--- resources/js/pages/admin/login.jsx
+++ resources/js/pages/admin/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
-const LoginPage = () => {
+const Login = () => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -148,7 +148,7 @@ const LoginPage = () => {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.95 }}
           >
-            <svg onClick={handleBackToHome} className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <svg className="w-5 h-5 text-white z-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
           </motion.button>
@@ -343,4 +343,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default Login;
