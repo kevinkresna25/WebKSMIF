38d224e68c6d66a0e0cab88da98a25909a0b14ee
Styaa <91019301+Styaa@users.noreply.github.com>
2025-08-26 19:34:13 +0700

    Revert "Update web.php"
    This reverts commit 7a87334485ef1ebca6eb0b5569bfd36ba1d3e690.

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 });
 
-Route::get('/login', fn() => redirect()->route('admin.login'))->name('login');
-
 /*
 |--------------------------------------------------------------------------
 | Protected Admin Routes
